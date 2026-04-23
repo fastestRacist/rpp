6faa6d6f3e8a54040ccaa9877a11c9e10757e4e9
Khobenkov <khobenkov@yandex.ru>
2026-04-23 21:47:21 +0700

    dodelal nemnogo lab 2

--- lab_2/lab_3_13.py
+++ lab_2/lab_3_13.py
@@ -1,11 +1,17 @@
 import random
 
-array = random.sample(range(100), 10)
-unique_values = set(array)
+array = random.choices(range(100), k=10)
 for i in range(len(array)):
     for j in range(len(array)):
         if i != j:
             if array[i] == array[j]:
                 print(f'Повторение элемента {array[i]} и {array[j]} с индексами {i}, {j}')
 
-    
+new_array = []
+for i in range(len(array)):
+    if array[i] < 15:
+        new_array.append(array[i]*2)
+    else:
+        new_array.append(array[i])
+print(array)
+print(new_array)
